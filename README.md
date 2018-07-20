dabb2e4a84a4e53fa0ee311a54b12b9786c5b2e2
MATSUBARA Nobutada <t12307043@gunma-u.ac.jp>
2018-07-20 10:33:35 +0900

    Add drone client type

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # drone-haskell
+
 Haskell client for the Drone API
+
+## USAGE
+
+```
+$ stack ghci
+>> :set -XOverloadedStrings -XOverloadedLabels
+>> import Data.Extensible
+>> import Network.HTTP.Req
+>> import Data.Default.Class
+>> token = ...
+>> client = HttpClient (#host @= "localhost" <: #token @= token <: nil)
+>> runReq def $ getLoginUser client
+>> rsp <- runReq def $ getLoginUser client
+>> responseBody rsp
+```
--- src/Drone/Client.hs
+++ src/Drone/Client.hs
@@ -1,11 +1,37 @@
-{-# LANGUAGE DataKinds    #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE OverloadedLabels  #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE TypeOperators     #-}
 
 module Drone.Client where
 
-import           Network.HTTP.Req (Option, Scheme, Url)
+import           Control.Lens     ((^.))
+import           Data.ByteString  (ByteString)
+import           Data.Extensible
+import           Data.Text        (Text)
+import           Network.HTTP.Req
+
+type BaseClient = Record
+   '[ "host"  >: Text
+    , "token" >: ByteString
+    ]
+
+newtype HttpClient = HttpClient BaseClient
+
+newtype HttpsClient = HttpsClient BaseClient
 
 class Client a where
   type ClientScheme a :: Scheme
   baseUrl :: a -> Url (ClientScheme a)
   mkHeader :: a -> Option scheme
+
+instance Client HttpClient where
+  type ClientScheme HttpClient = 'Http
+  baseUrl (HttpClient c) = http (c ^. #host) /: "api"
+  mkHeader (HttpClient c) = header "Authorization" ("Bearer " <> c ^. #token)
+
+instance Client HttpsClient where
+  type ClientScheme HttpsClient = 'Https
+  baseUrl (HttpsClient c) = https (c ^. #host) /: "api"
+  mkHeader (HttpsClient c) = header "Authorization" ("Bearer " <> c ^. #token)
